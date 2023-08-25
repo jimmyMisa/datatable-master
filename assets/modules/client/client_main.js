7b8c22cd7e04abaab0fc90fcf951d1deda503507
rafalimanana <rafalimananafabrice@gmail.com>
2023-08-25 15:55:22 +0300

    init reload

--- assets/modules/client/client_main.js
+++ assets/modules/client/client_main.js
@@ -3,40 +3,21 @@ import { C } from "vue/helper/V01Component";
 import { setChildView } from "vue/helper/renderVue.js";
 import { 
     ClientAssets as Assets, 
-    config 
+    config,
+    datatable
 } from "modules/client/ClientAssets.js";
 import {FIELD} from "common/structure/FIELD/FIELD.js"
 import { ClientApi } from "modules/client/classes/ClientApi.js";
 import { calculatePageNumbers } from "modules/common/datatableUtils.js";
 
 function main() {
-
     Assets.clientConfig().init();
     ClientApi.init();
-
     config().contentLines = {};
     if (!config().loadingContent) {
         config().loadingContent ={}
     }
-    config().loadingContent.isVisible = true;
-
-    var data = {
-        page:config().pagination.page,
-        size:config().pageSize.value,
-        orderBy:config().headerColumns.orderBy,
-        order:config().headerColumns.order,
-        key:config().searchInput.value,
-    }
-    var then = (result)=>{
-        var {datas=[], total=0, totalFiltered=0, size=10}= result;
-        config().contentLines = datas;
-        config().loadingContent.isVisible=false;
-        config().pageSize.field().value = size;
-        config().pagination.pages = calculatePageNumbers(totalFiltered, size);
-        config().instance.refresh()
-    }
-    ClientApi.listApi({data, then})
-
+    datatable().reload();
     setChildView("#app", ClientTable, {});
 }
 

f4cf86b7555d9702b80879e4f43584618c1c322f
AtrikaRa <arandriamanalina@passion-web.info>
2023-08-29 11:26:59 +0300

    update loading content

--- assets/modules/product/ProductConfig/ProductConfigContent/ProductConfigContent.js
+++ assets/modules/product/ProductConfig/ProductConfigContent/ProductConfigContent.js
@@ -7,6 +7,7 @@ import {
 class ProductConfigContent{
     static configure(){
         config().contentLines = ProductDatatableAssets.contentLines();
+        config().loadingContent = ProductDatatableAssets.loadingContent();
 
         //CONTENT COLUMNS ACTIONS
         config().editButton = ProductDatatableAssets.editButton();
